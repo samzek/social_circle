626451050d797ed99f3aa0721de71f6f1070a48e
samzek <samuele.zecchini92@gmail.com>
2015-06-05 14:09:13 +0200

    merge

--- SN/socialcircle/views.py
+++ SN/socialcircle/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpRequest,HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate,login
 from .models import SCUser,Post,Like,Cat
+
 from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        if "log_in" in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return render(request,'socialcircle/dashboard.html')
+                else:
+                    return render(request,'socialcircle/index.html')
+            else:
+                print "USER NOT FOUND"
+                return render(request,'socialcircle/index.html')
+        elif "sign" in request.POST:
+            return render(request,'socialcircle/registration.html')
+
     return render(request,'socialcircle/index.html')
+
 def dash(request,scuser_id):
     return render(request,'socialcircle/dashboard.html')
 def profile(request,scuser_id):
