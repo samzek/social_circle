f01aba6ae043f28e98170b2c2e7b7c144d77a84c
mive93 <micaelaverucchi@gmail.com>
2015-06-17 11:11:28 +0200

    modificato db inserita redirezione da localhost:8000 a localhost:8000/socialcirlce

--- SN/SN/urls.py
+++ SN/SN/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^socialcircle/', include('socialcircle.urls', namespace="socialcircle")),
     url(r'^admin/', include(admin.site.urls)),
 ]
