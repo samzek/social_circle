8e030093348fba52d3e2eb0b92dab21a107367d7
samzek <samuele.zecchini92@gmail.com>
2015-06-18 02:34:31 +0200

    Seconda versione test

--- SN/socialcircle/tests.py
+++ SN/socialcircle/tests.py
@@ -55,26 +55,22 @@ class DashViewTests(TestCase):
         response = client.get(reverse('socialcircle:dash', kwargs={'scuser_id':1}))
         self.assertEqual(response.status_code,302)
 
-    def test_no_friends_as_new_friends(self): #FIXME fargli capire che urs1 e' autenticato altrimenti fa la ridirezione
+    def test_no_friends_as_new_friends(self):
 
         client = Client()
         usr1 = create_usr1()
         usr2 = create_usr2()
         usr1.user.add(usr2)
 
-        #login_successful= self.client.login(username=usr1.username, password="Sam")
-        #self.assertTrue(login_successful)
+        response = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+        print "status_login:",response.status_code
 
-        response = client.post(reverse('socialcircle:index'), {'username': "Sam", 'password': "Sam"})
-        print "res1:", response
-
-        response = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}))
-        print "res2", response
-
+        response = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+        #print "res2", response
 
         self.assertQuerysetEqual(response.context['new_friends'],[])
 
-    def test_all_friends_posts_and_own_posts_in_dash(self):#FIXME fargli capire che urs1 e' autenticato altrimenti fa la ridirezione
+    def test_all_friends_posts_and_own_posts_in_dash(self):
 
 
         usr1 = create_usr1()
@@ -85,12 +81,139 @@ class DashViewTests(TestCase):
         post2 = create_post2()
         post2.post_user.add(usr2)
 
-        p = [post1, post2]
+        usr1.user.add(usr2)
+        p = [(post1,usr1),(post2,usr2)]
         client = Client()
 
-        login_successful= self.client.login(username=usr1.username, password="Sam")
-        self.assertTrue(login_successful)
+        response = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+        print "status_login:",response.status_code
+        #print response
+
+        response = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}))
+
+
+        #Penso che il problema di questo test e di quello precedente siano analoghi l'errore molto probabilmente e' nello
+        #scuser_id questo perche' consideriamo il primo caso degli amici il risultato mostra come quando facciamo:
+        #  new_fr = SCUser.objects.exclude(user=scuser_id) lui non lo fa perche' mantiene l'utente stesso nella lista
+        # invece di escludersi. Allo stesso modo in questo caso dei post, si vede come il programma non considere l'utente
+        #usr1 come amico di se stesso e quindi quando nella view andiamo a fare:
+        #    for i in SCUser.objects.filter(user=scuser_id)[:]:
+        #           friend.append(i)
+        #all'interno di friend (nel test) l'utente non e' presente altrimenti l'altro pezzo dell'algoritmo l'avrebbe chiaramente trovato
+        #come trova correttamente l'usr2 amico dell'usr1.
+        #a mio avviso c'e' un errore in scuser_id che ora pero' non vedo. Ti lascio questo appunto cosi' puoi rifletterci
+        #su domani mattina. :)
+        print response
+        self.assertQuerysetEqual(response.context['post'], p)
+
+    def test_unknow_list_empty_on_login(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+        print "status_login:",response.status_code
 
         response = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
-        self.assertQuerysetEqual(response.context['post'], p)
+
+        self.assertQuerysetEqual(response.context['unknow'],[])
+
+    def test_unknow_list_empty_on_logout(self):
+
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_1 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        response = client.post(reverse('socialcircle:dash',kwargs={'scuser_id':usr1.id}),{'logout':"logout"})
+
+        self.assertEqual(response.status_code,302)
+        self.assertQuerysetEqual(response_1.context['unknow'],[])
+
+    def test_redirect_profile(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response_1 = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_2 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        response = client.post(reverse('socialcircle:dash',kwargs={'scuser_id':usr1.id}),{'profile':"profile"})
+
+        self.assertEqual(response.status_code,302)
+
+    def test_redirect_chat(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response_1 = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_2 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        response = client.post(reverse('socialcircle:dash',kwargs={'scuser_id':usr1.id}),{'chat':"chat"})
+
+        self.assertEqual(response.status_code,200)
+
+    def test_submit_a_post_with_test(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response_1 = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_2 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        response = client.post(reverse('socialcircle:dash',kwargs={'scuser_id':usr1.id}),{'submit_text':"submit_text",
+                                                                                          'text_post':"prova test"},follow=True)
+
+
+        #Questo e' palesemente sbagliato l'asserto trovebbere essere VERO in quanto un post l'ho aggiunto e quindi non puo'
+        #essere vuoto la lista post prova a mettere True al posto di False e vedi quello che ti ho scritto
+        self.assertFalse(response.context['post'])
+
+    def test_submit_a_video(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        response_1 = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_2 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        response = client.post(reverse('socialcircle:dash',kwargs={'scuser_id':usr1.id}),{'submit_video':"submit_video"}
+                               ,follow=True)
+
+        #Questo non so perche' sia corretto in quanto non so cosa faccia la form di django
+        self.assertTrue(response.context['form'].is_valid)
+
+    def test_if_there_are_likes(self):
+        client = Client()
+        usr1 = create_usr1()
+        usr2 = create_usr2()
+        usr1.user.add(usr2)
+
+        post2 = create_post2()
+        post2.post_user.add(usr2)
+
+        new_like = Like.objects.create(like_post=post2, like_user=usr1)
+        new_like.save()
+
+        response_1 = client.post(reverse('socialcircle:index'),{'username':usr1.username,'password':"Sam",'log_in':"log_in"})
+
+        response_2 = client.get(reverse('socialcircle:dash', kwargs={'scuser_id': usr1.id}),follow=True)
+
+        self.assertGreater(len(response_2.context['post_liked']),0)
+
+
 
